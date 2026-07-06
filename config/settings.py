5026a1d0072f35071eb149e0e62a1d586a0bccc5
Serhat Birlikbaş <nsbirlikbas@gmail.com>
2026-07-06 22:43:05 +0300

    Sprint 0 - Foundation

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     "core",
+    "products",
 ]
 
 MIDDLEWARE = [
@@ -114,4 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
